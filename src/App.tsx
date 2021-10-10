351199cdcc7a342887ea1c9ae33b79b41a7c969b
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-10 23:55:12 +0300

    add main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+
+import { Row, Col, Skeleton, Steps, PageHeader } from "antd";
+
+import "html5-device-mockups/dist/device-mockups.css";
+import GetFormsInfo from "./GetFormsInfo";
+import { FormsEntity } from "./forms-declaration";
+import Forms from "./Forms";
+
+declare global {
+  interface Window {
+    mindbox: any;
+  }
+}
 
 function App() {
   useEffect(() => {
@@ -20,22 +34,69 @@ function App() {
 
     
   }, []);
+
+  const { Step } = Steps;
+
+  const [currentStep, setCurrentStep] = useState<number>(1);
+
+  const [forms, setForms] = useState<FormsEntity[]>([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row gutter={{ xs: 8, sm: 16, md: 24 }}>
+        <Col span={8}>
+          <div className="device-wrapper">
+            <div
+              className="device"
+              data-device="iPhone7"
+              data-orientation="portrait"
+              data-color="black"
+            >
+              <div
+                className="screen"
+                style={{ backgroundColor: "white", padding: "15px 10px" }}
+              >
+                <Skeleton />
+              </div>
+              <div className="button">
+                {/* <!-- You can hook the "home button" to some JavaScript events or just remove it --> */}
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col span={16}>
+          <PageHeader
+            className="site-page-header"
+            title="Пример персонализации приложения"
+          />
+          <Row gutter={{ xs: 8, sm: 16, md: 24 }}>
+            <Col span={8}>
+              <Steps
+                direction="vertical"
+                current={currentStep}
+                onChange={setCurrentStep}
+              >
+                <Step
+                  title="Получить формы"
+                  description="Получить список настроенных форм, выполнив GET запрос по адресу {pam-pam}"
+                />
+                <Step
+                  title="Проверить таргетинг"
+                  description="Для каждой формы может быть настрое натргетинг. Надо проверить, подходит ли пользователь под заданные условия"
+                />
+                <Step
+                  title="Принять решение об отображении"
+                  description="Если пользователь подходит под условия, надо отобразить ему настроенный контент"
+                />
+              </Steps>
+            </Col>
+            <Col span={16}>
+              <GetFormsInfo updateFormsInfo={setForms} />
+              {forms && <Forms forms={forms} />}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 }
