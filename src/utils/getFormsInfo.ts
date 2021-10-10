11730364c9a8fde8ccc5f529b6d4565a1e4bbd18
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-10 23:54:34 +0300

    fix type of clientId arg

--- src/utils/getFormsInfo.ts
+++ src/utils/getFormsInfo.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Forms } from "../forms-declaration";
 
-const getFormsInfo = async (clientId: number, domain: string) => {
+const getFormsInfo = async (clientId: string, domain: string) => {
   const url = `https://web.popmechanic.ru/web/init/${clientId}/?domain=${domain}`;
 
   const result = await axios.get<Forms>(url);
