fd769aec0974bea156c93189850ada23f5a8fdc6
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-10 23:55:00 +0300

    add mindbox

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    window.mindbox =
+      window.mindbox ||
+      function () {
+        window.mindbox.queue.push(arguments);
+      };
+    window.mindbox.queue = window.mindbox.queue || [];
+    window.mindbox("create", {
+      endpointId: "wpush-test",
+    });
+
+    var script = document.createElement("script");
+    script.src = "https://api.mindbox.ru/scripts/v1/tracker.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
