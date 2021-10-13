19eef114c5a00e1555ad7b138cf8b68ad173ee8d
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-13 21:37:17 +0300

    extract steps to component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 
-import { Row, Col, Skeleton, Steps, PageHeader } from "antd";
+import { Row, Col, PageHeader } from "antd";
 
 import { GetFormsInfo } from "pages/request-constructor";
-import { FormsEntity } from "forms-declaration";
-import { Forms } from "pages/preview-of-forms";
+import { FormsEntity } from "shared/api/get-personalization-data/model";
+
 import ResultView from "./result-view/ui";
+import { StepsIndicator } from "entities/steps-indicator";
+
+import { Forms } from "shared/api/get-personalization-data";
 
 const PersonalizationTester = () => {
-  const { Step } = Steps;
-
   const [currentStep, setCurrentStep] = useState<number>(1);
 
-  const [forms, setForms] = useState<FormsEntity[]>([]);
+  const [personalizationData, setPersonalizationData] = useState<{
+    parsed: FormsEntity[];
+    raw: Forms;
+  }>();
 
   return (
     <div>
@@ -27,24 +31,7 @@ const PersonalizationTester = () => {
           />
           <Row gutter={{ xs: 8, sm: 16, md: 24 }}>
             <Col span={8}>
-              <Steps
-                direction="vertical"
-                current={currentStep}
-                onChange={setCurrentStep}
-              >
-                <Step
-                  title="Получить формы"
-                  description="Получить список настроенных форм, выполнив GET запрос по адресу {pam-pam}"
-                />
-                <Step
-                  title="Проверить таргетинг"
-                  description="Для каждой формы может быть настрое натргетинг. Надо проверить, подходит ли пользователь под заданные условия"
-                />
-                <Step
-                  title="Принять решение об отображении"
-                  description="Если пользователь подходит под условия, надо отобразить ему настроенный контент"
-                />
-              </Steps>
+              <StepsIndicator currentStep={currentStep} />
             </Col>
             <Col span={16}>
               <GetFormsInfo updateFormsInfo={setForms} />
