2c354ea58189f8862688fe9d7a7812c1ee1fb99f
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-13 23:56:36 +0300

    add raw preview

---
+++ src/entities/preview-raw/model.ts
@@ -0,0 +1,5 @@
+import { FormsEntity } from "processes/get-personalization-data";
+
+export type PreviewRawProps = {
+  rawData: FormsEntity[]
+}
---
+++ src/entities/preview-raw/ui.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import { PreviewRawProps } from "./model";
+
+export const PreviewRaw = ({ rawData }: PreviewRawProps) => {
+  const valueFormPreview =
+    rawData.length > 0 ? JSON.stringify(rawData, null, 2) : "";
+
+  return (
+    <AceEditor
+      mode="json"
+      theme="github"
+      value={valueFormPreview}
+      name="PREVIEW_RAW_PERSONALIZATION_DATA"
+      editorProps={{ $blockScrolling: true }}
+    />
+  );
+};
