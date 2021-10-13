3e7a9dfa71e19a8d86f346987a0bb5026f2c1cf6
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-13 22:38:08 +0300

    move to precesses

--- src/processes/get-personalization-data/getData.ts
+++ src/processes/get-personalization-data/getData.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Forms } from "shared/api/get-personalization-data/model";
+import { Forms } from "processes/get-personalization-data/model";
 
 export const getData = async (clientId: string, domain: string) => {
   const url = `https://web.popmechanic.ru/web/init/${clientId}/?domain=${domain}`;
