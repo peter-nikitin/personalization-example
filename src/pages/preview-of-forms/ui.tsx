eb8495a41908345d4e09e17a8f2782b38cf2ca5b
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-13 21:37:43 +0300

    small refactors

--- src/pages/preview-of-forms/ui.tsx
+++ src/pages/preview-of-forms/ui.tsx
@@ -1,6 +1,9 @@
 import { Row, Typography } from "antd";
-import { FormsEntity } from "forms-declaration";
+import { FormsEntity } from "shared/api/get-personalization-data/model";
 import { FormCard } from "entities/form-card";
+import { TogglePreviewMode } from "features/toggle-preview";
+import { PREVIEW_MODS } from "features/toggle-preview/model";
+import React, { useState } from "react";
 
 type FormsProps = {
   forms: FormsEntity[];
@@ -9,9 +12,12 @@ type FormsProps = {
 export const Forms = ({ forms }: FormsProps) => {
   const { Title } = Typography;
 
+  const [previewMode, setPeviewMode] = useState(PREVIEW_MODS.parsed);
+
   return (
     <div>
       <Title level={4}>Настроенные формы</Title>
+      <TogglePreviewMode changeMode={setPeviewMode} currentMode={previewMode} />
       <Row gutter={16} wrap={true}>
         {forms.map((form) => (
           <FormCard key={form.id} formInfo={form} />
