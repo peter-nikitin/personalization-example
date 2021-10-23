ab454e75b99dcb977517b522a5ef211d03f8e3f6
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-23 20:17:46 +0300

    add changing step

--- src/entities/personalization-data/model.ts
+++ src/entities/personalization-data/model.ts
@@ -1,8 +1,12 @@
 import { createEffect, createStore } from "effector";
-import { getData } from "processes/get-personalization-data";
+import { setStep } from "entities/steps-indicator/model";
+import { FormsEntity, getData } from "processes/get-personalization-data";
 
-export const $personalizationData = createStore({});
+export const $personalizationData = createStore<FormsEntity[]>([]);
 
 export const getDataFx = createEffect(getData);
 
-$personalizationData.on(getDataFx.doneData, (_state, result) => result);
+$personalizationData.on(getDataFx.doneData, (_state, result) => {
+  setStep(1);
+  return result;
+});
