56fb81e3b5b6a72823102f73a480f02a54c0e75f
Petr Nikitin <nikitin.p.94@gmail.com>
2021-10-23 19:19:08 +0300

    made arguments type of constructor stor

--- src/processes/get-personalization-data/getData.ts
+++ src/processes/get-personalization-data/getData.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { Forms } from "processes/get-personalization-data/model";
+import { Forms, GetData } from "processes/get-personalization-data/model";
 
-export const getData = async (clientId: string, domain: string) => {
-  const url = `https://web.popmechanic.ru/web/init/${clientId}/?domain=${domain}`;
+export const getData: GetData = async ({ id, domain }) => {
+  const url = `https://web.popmechanic.ru/web/init/${id}/?domain=${domain}`;
 
   const result = await axios.get<Forms>(url);
 
